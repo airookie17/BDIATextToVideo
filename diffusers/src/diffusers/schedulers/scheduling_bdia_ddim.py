a353697b69632b555e5c7bf0829277133613e58f
airookie17 <surisetti.komal@gmail.com>
2024-07-30 12:13:57 +0100

    Update scheduling_bdia_ddim.py

--- diffusers/src/diffusers/schedulers/scheduling_bdia_ddim.py
+++ diffusers/src/diffusers/schedulers/scheduling_bdia_ddim.py
@@ -14,19 +14,22 @@ class BDIADDIMScheduler(DDIMScheduler):
         alpha_prod_t = self.alphas_cumprod[timestep]
         alpha_prod_t_prev = self.alphas_cumprod[prev_timestep] if prev_timestep >= 0 else self.final_alpha_cumprod
         beta_prod_t = 1 - alpha_prod_t
-        pred_original_sample = (sample - beta_prod_t ** (0.5) * model_output) / alpha_prod_t ** (0.5)
-        pred_sample_direction = (1 - alpha_prod_t_prev) ** (0.5) * model_output
-        prev_sample = alpha_prod_t_prev ** (0.5) * pred_original_sample + pred_sample_direction
+        sigma_t = beta_prod_t ** 0.5
+        sigma_t_prev = (1 - alpha_prod_t_prev) ** 0.5
 
-        # BDIA modifications
+        pred_original_sample = (sample - sigma_t * model_output) / (alpha_prod_t ** 0.5)
+
         if self.x_last is not None:
             alpha_prod_t_last = self.alphas_cumprod[self.t_last] if self.t_last is not None else alpha_prod_t
+            sigma_t_last = (1 - alpha_prod_t_last) ** 0.5
+
             prev_sample = (
-                self.x_last
-                - (1 - self.gamma) * (self.x_last - sample)
-                - self.gamma * (alpha_prod_t_last ** 0.5 * pred_original_sample + (1 - alpha_prod_t_last) ** 0.5 * model_output - sample)
-                + alpha_prod_t_prev ** 0.5 * pred_original_sample + pred_sample_direction - sample
+                    self.gamma * self.x_last
+                    - self.gamma * (alpha_prod_t_last ** 0.5 * pred_original_sample + sigma_t_last * model_output)
+                    + alpha_prod_t_prev ** 0.5 * pred_original_sample + sigma_t_prev * model_output
             )
+        else:
+            prev_sample = alpha_prod_t_prev ** 0.5 * pred_original_sample + sigma_t_prev * model_output
 
         self.x_last = sample
         self.t_last = timestep
@@ -35,7 +38,6 @@ class BDIADDIMScheduler(DDIMScheduler):
             return (prev_sample,)
 
         return DDIMSchedulerOutput(prev_sample=prev_sample, pred_original_sample=pred_original_sample)
-
     def set_timesteps(self, num_inference_steps, device="cpu"):
         super().set_timesteps(num_inference_steps, device)
         self.x_last = None
