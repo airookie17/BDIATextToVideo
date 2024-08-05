24deecf878f4a6383d745db564bca14fc51f051d
Sai Komal Surisetti <143885466+airookie17@users.noreply.github.com>
2024-08-06 03:33:41 +0530

    Update scheduling_bdia_ddim.py

--- diffusers/src/diffusers/schedulers/scheduling_bdia_ddim.py
+++ diffusers/src/diffusers/schedulers/scheduling_bdia_ddim.py
@@ -1,5 +1,8 @@
 from .scheduling_ddim import DDIMScheduler, DDIMSchedulerOutput
 import torch
+import numpy as np
+from typing import Optional, Union, Tuple, List
+
 
 class BDIADDIMScheduler(DDIMScheduler):
     def __init__(self, *args, **kwargs):
@@ -8,48 +11,68 @@ class BDIADDIMScheduler(DDIMScheduler):
         self.t_last = None  # Tracks the last timestep
         self.gamma = kwargs.get('gamma', 0.5)  # Default gamma value
 
-    def step(self, model_output, timestep, sample, return_dict=True):
-        # Find the index of the current timestep
-        step_index = torch.where(self.timesteps == timestep)[0].item()
-        # Determine the previous timestep
-        prev_timestep = self.timesteps[step_index + 1] if step_index < len(self.timesteps) - 1 else 0
+    def set_timesteps(self, num_inference_steps: int):
+        self.num_inference_steps = num_inference_steps
+        self.timesteps = np.arange(0, self.num_train_timesteps, self.num_train_timesteps // self.num_inference_steps)[::-1]
+        self.x_last = None
+        self.t_last = None
 
-        # Compute alpha and sigma values for the current and previous timesteps
-        alpha_prod_t = self.alphas_cumprod[timestep].to(sample.device)
-        alpha_prod_t_prev = self.alphas_cumprod[prev_timestep].to(sample.device) if prev_timestep >= 0 else self.final_alpha_cumprod.to(sample.device)
+    def step(
+        self,
+        model_output: torch.FloatTensor,
+        timestep: int,
+        sample: torch.FloatTensor,
+        eta: float = 0.0,
+        use_clipped_model_output: bool = False,
+        generator: Optional[torch.Generator] = None,
+        variance_noise: Optional[torch.FloatTensor] = None,
+        return_dict: bool = True,
+    ) -> Union[DDIMSchedulerOutput, Tuple]:
+        prev_timestep = timestep - self.num_train_timesteps // self.num_inference_steps
+
+        alpha_prod_t = self.alphas_cumprod[timestep]
+        alpha_prod_t_prev = self.alphas_cumprod[prev_timestep] if prev_timestep >= 0 else self.final_alpha_cumprod
         beta_prod_t = 1 - alpha_prod_t
-        sigma_t = beta_prod_t ** 0.5
-        sigma_t_prev = (1 - alpha_prod_t_prev) ** 0.5
+        beta_prod_t_prev = 1 - alpha_prod_t_prev
 
-        # Predict the original sample (x_0)
-        pred_original_sample = (sample - sigma_t * model_output) / (alpha_prod_t ** 0.5)
+        if self.prediction_type == "epsilon":
+            pred_original_sample = (sample - beta_prod_t**0.5 * model_output) / alpha_prod_t**0.5
+        elif self.prediction_type == "sample":
+            pred_original_sample = model_output
+        elif self.prediction_type == "v_prediction":
+            pred_original_sample = (alpha_prod_t**0.5) * sample - (beta_prod_t**0.5) * model_output
 
-        # Implement the BDIA-DDIM update logic
+        if self.thresholding:
+            pred_original_sample = self._threshold_sample(pred_original_sample)
+
+        variance = 0
+        if eta > 0:
+            variance = self._get_variance(timestep, prev_timestep)
+            if variance_noise is None:
+                variance_noise = torch.randn_like(model_output, generator=generator)
+            variance = eta * variance**0.5 * variance_noise
+
+        if use_clipped_model_output:
+            model_output = (sample - alpha_prod_t**0.5 * pred_original_sample) / beta_prod_t**0.5
+            pred_original_sample = torch.clamp(pred_original_sample, -1, 1)
+
         if self.x_last is not None:
-            # Compute alpha_prod_t_last for the previous timestep (a_last)
-            alpha_prod_t_last = self.alphas_cumprod[self.timesteps[step_index + 1]].to(sample.device) if step_index < len(self.timesteps) - 1 else alpha_prod_t
-            sigma_t_last = (1 - alpha_prod_t_last) ** 0.5
-
-            # BDIA-DDIM update step
-            prev_sample = (
-                self.gamma * self.x_last
-                - self.gamma * (alpha_prod_t_last ** 0.5 * pred_original_sample + sigma_t_last * model_output)
-                + alpha_prod_t_prev ** 0.5 * pred_original_sample + sigma_t_prev * model_output
-            )
+            a_last = self.alphas_cumprod[timestep + 1] if timestep + 1 < len(self.alphas_cumprod) else self.final_alpha_cumprod
+            x_prev = (self.x_last - (1 - self.gamma) * (self.x_last - sample)
+                      - self.gamma * (a_last**0.5 * pred_original_sample + (1 - a_last)**0.5 * model_output - sample)
+                      + alpha_prod_t_prev**0.5 * pred_original_sample + (1 - alpha_prod_t_prev - variance)**0.5 * model_output - sample)
         else:
-            # Standard DDIM update if no previous sample is available
-            prev_sample = alpha_prod_t_prev ** 0.5 * pred_original_sample + sigma_t_prev * model_output
+            x_prev = alpha_prod_t_prev**0.5 * pred_original_sample + (1 - alpha_prod_t_prev - variance)**0.5 * model_output + variance
 
-        # Update the state for the next step
         self.x_last = sample
         self.t_last = timestep
 
         if not return_dict:
-            return prev_sample,
+            return (x_prev,)
 
-        return DDIMSchedulerOutput(prev_sample=prev_sample, pred_original_sample=pred_original_sample)
+        return DDIMSchedulerOutput(prev_sample=x_prev, pred_original_sample=pred_original_sample)
 
-    def set_timesteps(self, num_inference_steps, device="cpu"):
-        super().set_timesteps(num_inference_steps, device)
+    def reset(self):
         self.x_last = None
         self.t_last = None
+
